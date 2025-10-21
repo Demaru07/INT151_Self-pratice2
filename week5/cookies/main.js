80954966f6a7c4e634045de6dc1a501826091fe6
Jaturapat <189746489+Demaru07@users.noreply.github.com>
2025-10-22 01:32:40 +0700

    week5

---
+++ week5/cookies/main.js
@@ -0,0 +1,9 @@
+console.log(document.cookie);
+document.cookie = `theme=dark;expires=${new Date (2025, 9, 15)}`
+console.log(document.cookie);
+
+// document.cookie = "font-size = 14px; max-age = 60"
+// console.log(document.cookie);
+
+document.cookie = "theme=light"
+document.cookie = "color=warm"
